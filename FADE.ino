3ea7327e1965ad11219ac76301dc403f351ab996
preethamh195 <71484419+preethamh195@users.noreply.github.com>
2021-10-22 15:38:29 +0530

    Add files via upload

---
+++ FADE.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(9,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int i;
+  for(i=0;i<=255;i=i+5){
+    analogWrite(9,i);
+    delay(100);
+  }
+  for(i=255;i>=0;i=i-5){
+    analogWrite(9,i);
+    delay(100);
+  }
+}
