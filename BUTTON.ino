c06626d4e693b70efc1bb1e8be0b777291fa827a
preethamh195 <71484419+preethamh195@users.noreply.github.com>
2021-10-22 15:13:17 +0530

    Add files via upload

---
+++ BUTTON.ino
@@ -0,0 +1,19 @@
+int button=2;
+int led=13;
+int buttonstate=0;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(button,INPUT);
+  pinMode(led,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  buttonstate=digitalRead(button);
+  if(buttonstate==HIGH){
+    digitalWrite(led,HIGH);
+  }
+  else{
+    digitalWrite(led,LOW);
+  }
+}
---
+++ L1.ino
@@ -0,0 +1,25 @@
+int red=13;
+int yellow=10;
+int green=8;
+void setup() {
+  // put your setup code here, to run once:
+pinMode (red,OUTPUT);
+pinMode (yellow,OUTPUT);
+pinMode (green,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ digitalWrite (red,HIGH);
+ digitalWrite(yellow,LOW);
+ digitalWrite(green,LOW);
+ delay(90);
+ digitalWrite(red,LOW);
+ digitalWrite (yellow,HIGH);
+ digitalWrite(green,LOW);
+ delay(900);
+ digitalWrite(red,LOW);
+ digitalWrite(yellow,LOW);
+ digitalWrite (green,HIGH);
+ delay(9);
+}
